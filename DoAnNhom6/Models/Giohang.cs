c1ad48ee22c6c44b3d18b5fb25579bf11f5badb4
Nguyễn Anh Đức <naducc4pbchau@gmail.com>
2022-06-15 03:37:59 +0700

    webstore

---
+++ DoAnNhom6/Models/Giohang.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DoAnNhom6.Models
+{
+    public class Giohang
+    {
+        MyDataDataContext data = new MyDataDataContext();
+        public int id { get; set; }
+        [Display(Name = "Tên")]
+        public string ten { get; set; }
+        [Display(Name = "Ảnh bìa")]
+        public string hinh { get; set; }
+        [Display(Name = "Giá bán")]
+        public Double gia { get; set; }
+        [Display(Name = "Số lượng")]
+        public int iSoluong { get; set; }
+        [Display(Name = "Thành tiền")]
+        public Double dThanhtien
+        {
+            get { return iSoluong * gia; }
+        }
+        public Giohang(int id)
+        {
+            this.id = id;
+            DongHo dongho = data.DongHos.Single(n => n.id == id);
+            ten = dongho.ten;
+            hinh = dongho.hinh;
+            gia = double.Parse(dongho.gia.ToString());
+            iSoluong = 1;
+        }
+    }
+}
