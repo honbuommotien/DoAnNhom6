bc31b3ab1d9746a18b84aafcd5ba2257e43a7c92
Le Quang Huy <qu4nghuyle@gmail.com>
2022-06-25 17:15:52 +0700

    ...

--- DoAnNhom6/App_Start/RouteConfig.cs
+++ DoAnNhom6/App_Start/RouteConfig.cs
@@ -14,6 +14,84 @@ namespace DoAnNhom6
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "ListDongHo",
+                url: "he-thong",
+                defaults: new { controller = "DongHo", action = "ListDongHo", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "About",
+                url: "gioi-thieu",
+                defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Contact",
+                url: "lien-he",
+                defaults: new { controller = "Home", action = "Contact", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "DetailDongHo",
+                url: "chi-tiet-sp-dong-ho-{id}",
+                defaults: new { controller = "DongHo", action = "Detail", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "CreateDongHo",
+                url: "tao-sp-moi",
+                defaults: new { controller = "DongHo", action = "Create", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "EditDongHo",
+                url: "edit-tt-sp-dong-ho-{id}",
+                defaults: new { controller = "DongHo", action = "Edit", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "DeleteDongHo",
+                url: "xoa-sp-dong-ho-{id}",
+                defaults: new { controller = "DongHo", action = "Delete", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Home",
+                url: "trang-chu",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "DangKy",
+                url: "dang-ky-tai-khoan",
+                defaults: new { controller = "NguoiDung", action = "DangKy", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "DangNhap",
+                url: "dang-nhap",
+                defaults: new { controller = "NguoiDung", action = "DangNhap", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "GioHang",
+                url: "gio-hang",
+                defaults: new { controller = "GioHang", action = "GioHang", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "DatHang",
+                url: "dat-hang",
+                defaults: new { controller = "GioHang", action = "DatHang", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "XacNhanDonHang",
+                url: "xac-nhan-don-hang",
+                defaults: new { controller = "GioHang", action = "XacNhanDonHang", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
